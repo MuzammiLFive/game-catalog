760d4c755ae9dd93e883d0ee28c098fd18f107d8
MuzammiLFive <muzammil@WORKSTATION.localdomain>
2020-09-06 16:52:09 +0530

    working app 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 import { GamesService } from './shared/games.service';
 import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, CategoriesComponent, HomeComponent, AboutComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    CategoriesComponent,
+    HomeComponent,
+    AboutComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [GamesService],
   bootstrap: [AppComponent],
 })
--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { GamesService } from '../shared/games.service';
+import { Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+  styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent implements OnInit {
+  constructor(private gamesService: GamesService) {}
 
-  constructor() { }
-
+  gamesList = null;
+  games = null;
   ngOnInit(): void {
+    this.gamesService.all().subscribe((list) => (this.gamesList = list));
   }
 
+  selectedGenre(genre) {
+    this.games = genre.games;
+  }
+
+  Unset() {
+    this.games = null;
+  }
 }
